34c1a9463eb8dd9b51a2de329bfe9101e0fed78b
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-12-27 17:27:45 +0530

    commit

---
+++ assign_zero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == 0)
+        {
+            cout<<i+1;
+        }
+        
+    }
+    
+    
+    return 0;
+}
