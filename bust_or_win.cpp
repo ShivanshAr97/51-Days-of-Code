98d6c54065fcf937e9a1b85262bd71e73ba8f463
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-12-30 13:29:40 +0530

    Day-1

---
+++ bust_or_win.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	int a,b,c;
+    cin>>a>>b>>c;
+    if((a+b+c)>=22)
+        cout<<"bust";
+    else
+    cout<<"win";
+    return 0;
+}
