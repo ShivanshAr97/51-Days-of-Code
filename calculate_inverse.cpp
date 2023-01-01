7e1cb08642b322428b3b371e7b9e7dc28a7e9c6b
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-01 13:55:42 +0530

    commit

---
+++ calculate_inverse.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    double ans = 0;
+    double arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        double res = 1 / arr[i];
+        ans += res;
+    }
+    cout << fixed << setprecision(8) << 1/ans;
+
+    return 0;
+}
