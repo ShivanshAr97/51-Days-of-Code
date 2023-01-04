72bf2772ac85488dbd48509c9defae7894ff039e
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-04 23:50:06 +0530

    commit

---
+++ stringram.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long n;
+    cin >> n;
+    map<string, long> mp;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        mp[s]++;
+    }
+    long count = 0;
+    for (auto &i : mp)
+    {
+        count = count + ((i.second * (i.second - 1)) / 2);
+    }
+    cout << count;
+}
