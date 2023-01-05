b9ac08afc5fe17a894e09cfc5fb62807d31624db
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-05 13:01:10 +0530

    commit

---
+++ christmas_gift_or_new_year_gift.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    for (int i = b-a+1; i < b+a; i++)
+    {
+        cout<<i<<" ";
+    }
+    
+    return 0;
+}
