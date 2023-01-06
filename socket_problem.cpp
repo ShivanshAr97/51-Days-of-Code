b97367296d68a6f36cc80e4b38f5749a9a208dfa
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-06 23:38:42 +0530

    commit

---
+++ socket_problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    int sockets = 1;
+    int cords = 0;
+    while(sockets<B){
+        cords++;
+        sockets=sockets-1+A;
+    }
+
+    cout<<cords;
+
+    return 0;
+}
