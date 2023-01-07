53698aee5bfe9c83d69c61033db7b25c7fffb099
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-07 23:56:48 +0530

    commit

---
+++ max_modperm.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	int n;
+    cin>>n;
+    int ans = ((n)*(n-1))/2;
+    cout<<ans;
+    return 0;
+}
