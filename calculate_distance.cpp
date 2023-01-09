b5ae223f1e30eb18f4e57e76028fdb8f1bae4235
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-09 12:42:09 +0530

    commit

---
+++ calculate_distance.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if (a>=b && a>=c)
+    {
+        cout<<b+c;
+    }
+    else if (b>=a && b>=c)
+    {
+        cout<<a+c;
+    }
+    else{
+        cout<<a+b;
+    }
+    
+    
+    return 0;
+}
