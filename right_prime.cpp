00763caa7f222e3b468b2de0d9dd6fc5bee22345
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-13 14:30:29 +0530

    commit

---
+++ right_prime.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+bool isPrime(int x) {
+    if (x <= 1) return false;
+    for (int i = 2; i <= sqrt(x); i++) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+
+int findNextPrime(int x) {
+    while (!isPrime(x)) x++;
+    return x;
+}
+
+int main() {
+    int x;
+    cin >> x;
+    cout<<findNextPrime(x);
+    return 0;
+}
