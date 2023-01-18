129c1573dca31adb0c259cc15f1dcfdbf8d3a808
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-01-18 13:54:46 +0530

    commit

---
+++ edward_and_maths_competition.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int even = floor(a/2);
+    int odd = a-even;
+    cout<<even*odd;
+    
+    return 0;
+}
