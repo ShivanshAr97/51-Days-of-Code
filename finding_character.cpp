c485ca3c5bb73ad58777e25512373edd159fb64d
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-02-26 13:13:20 +0530

    commit

---
+++ finding_character.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	float n,x;
+    cin>>n>>x;
+    cout<<char(64+ceil((x/n)));
+    return 0;
+}
