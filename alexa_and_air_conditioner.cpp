c6eac8527ccaf92ce22f44848d8482550dbd5729
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-01 12:06:35 +0530

    commit

---
+++ alexa_and_air_conditioner.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	int n;
+    cin>>n;
+    if(n>=30){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
