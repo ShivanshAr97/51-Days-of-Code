fcd4d4b488b5691f44729533619b18737295d278
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-06 12:18:22 +0530

    commit

---
+++ emilia_and_equalities.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	int a,b,c;
+    cin>>a>>b>>c;
+    if((a*a+b*b)<c*c){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
