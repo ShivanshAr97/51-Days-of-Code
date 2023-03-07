a346b96b9a32d8b890e7213b7d5e1865dce4849e
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-07 22:18:49 +0530

    commit

---
+++ spinning_slots.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	string s;
+    cin>>s;
+    if(s[0] == s[1] && s[1] == s[2]){
+        cout<<"Won";
+    }
+    else{
+        cout<<"Lost";
+    }
+    return 0;
+}
