385420d3b7b2ffdf677d4147c362fb81f8927aae
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-15 11:51:55 +0530

    commit

---
+++ alexa_and_drink.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	float a,b;
+    cin>>a>>b;
+    cout<<fixed<<setprecision(2);
+    cout<<((a*b)/100);
+    return 0;
+}
