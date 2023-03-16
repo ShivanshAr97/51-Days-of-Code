23be1fbad50ab6b7d84b37811a16e6d660a2b4d5
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-16 10:58:47 +0530

    commit

---
+++ meet_in_time.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	int d,t,s;
+    cin>>d>>t>>s;
+    if((d/t)<=s){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
