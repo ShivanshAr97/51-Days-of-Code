ee48bec043d3dd3a66dabe87fb53fabbb64c4841
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-17 22:00:29 +0530

    commit

---
+++ palindromic_string.java
@@ -0,0 +1,38 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    static boolean checkPalindrome(String str,int l,int r){
+        while(l<r){
+            if(str.charAt(l)!=str.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    static boolean helper(String str,int l,int r){
+        while(l<r){
+            if(str.charAt(l)=='a'&&str.charAt(r)=='a'){
+                l++;
+                r--;
+            }
+            else break;
+        }
+ while(l<r&&str.charAt(r)=='a'){
+            r--;
+        }
+        return checkPalindrome(str,l,r);
+    } 
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        boolean result=helper(str,0,str.length()-1);
+        if(result)System.out.print("Yes");
+        else System.out.print("No");
+    }
+}
