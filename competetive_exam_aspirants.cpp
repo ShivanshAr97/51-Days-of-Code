71c1f16ae61d8dca0f504b689b81bdd2c5a536ec
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-03-25 11:19:59 +0530

    commit

---
+++ competetive_exam_aspirants.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+	int a,b,c;
+    cin>>a>>b>>c;
+    if((c>=b) && (c<=a)){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
